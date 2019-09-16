8d3667187924d60d38148e1a8e60c7bc0c0c5644
lyxbyr <13021939199@163.com>
2019-09-17 07:39:46 +0800

    add Search_in_Rotated_Sorted_ArrayII_81.cpp

---
+++ Search_in_Rotated_Sorted_ArrayII_81.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+    int first = 0, last = nums.size() - 1;
+    while (first <= last) {
+        const int mid = first + (last - first) / 2;
+        if (nums[mid] == target)
+            return true;
+        if (nums[first] < nums[mid]) {
+            if (nums[first] <= target && target < nums[mid]) 
+                last = mid -1;
+            else
+                first = mid + 1;
+        } else if (nums[first] > nums[mid]) {
+            if (nums[mid] < target && target <= nums[last])
+                first = mid + 1;
+            else
+                last = mid - 1;   
+        } else
+            first++;       
+      }
+      return false;     
+    }
+};
